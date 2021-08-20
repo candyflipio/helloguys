4548d8b1d61426c9028f4165f7ac9c7b4f7a0c8e
candyflipio <chuhmanenkoaa@gmail.com>
2021-08-20 10:30:28 +0300

    Initial Commit

---
+++ G60L3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  G60L3
+//
+//  Created by pro on 20.08.2021.
+//  Copyright © 2021 pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
